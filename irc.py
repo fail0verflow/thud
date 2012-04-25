730c5b92b7ecb4751c1ac7846a584784b6b9d5d1
c1de0x <c1de0x@gmail.com>
2012-04-25 06:31:07 -0700

    thudshell: add list command

--- irc.py
+++ irc.py
@@ -76,11 +76,32 @@ class HelpCommand(ThudCommand):
                 self.shell.commands[cmd].help(args[1:])
             else:
                 self.shell.respond("help: unknown command")
+class ListCommand(ThudCommand):
+    def __init__(self,shell):
+        super(ListCommand,self).__init__(shell,"list","list things (servers, client-resources, etc)")
+    def _run(self,args):
+        if len(args) != 1:
+            return self.help(args)
+        kind = args[0].lower()
+        if "servers".startswith(kind) or "upstreams".startswith(kind):
+            self.shell.respond("connected upstream servers:")
+            upstream_connections = self.shell.cache.upstream.config.user.upstream_connections
+            for name, upstream in upstream_connections.items():
+                self.shell.respond("    %s - %s" % (name, upstream.config.uri))
+        elif "clients".startswith(kind) or "resources".startswith(kind):
+            self.shell.respond("connected downstream client resources:")
+            clients = self.shell.cache.upstream.config.user.clients
+            for resource, client in clients.items():
+                self.shell.respond("    %s connected to %s last seen at %s" % (resource,client.upstreamref,self.shell.cache.last_seen[resource]))
+
 
 class ThudShell(object):
     def __init__(self, cache, client):
         self.cache,self.client = cache,client
-        self.commands = {"help": HelpCommand(self)}
+        self.commands = {
+            "help": HelpCommand(self),
+            "list": ListCommand(self),
+        }
     def respond(self, message):
         messages = message.split("\n")
         for m in messages:
